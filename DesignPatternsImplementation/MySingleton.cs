a18cdda768c7d10685bab6ad9fbc57728cf45c4e
Clay Aldas <clayaldas@gmail.com>
2025-10-21 12:15:01 -0500

    Proyecto de patrones de diseño creado

---
+++ DesignPatternsImplementation/MySingleton.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatternsImplementation;
+
+public class MySingleton
+{
+    private static MySingleton _instance;
+
+    public Guid Id { get; private set; }
+    private MySingleton()
+    {
+        Id = Guid.NewGuid();
+    }
+    public static MySingleton GetInstance()
+    {
+        if ( _instance == null )
+        {
+            _instance = new MySingleton();
+        }
+
+        return _instance;
+    }
+}
---
+++ DesignPatternsImplementation/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DesignPatternsImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Patrón creacional: Singleton!");
+            Console.WriteLine();
+
+            //MySingleton mySingleton = new MySingleton();
+            MySingleton instance1 = MySingleton.GetInstance();
+            Console.WriteLine($"Intancia 1: {instance1.Id}");
+
+            MySingleton instance2 = MySingleton.GetInstance();
+            Console.WriteLine($"Intancia 2: {instance2.Id}");
+
+            MySingleton instance3 = MySingleton.GetInstance();
+            Console.WriteLine($"Intancia 3: {instance3.Id}");
+
+            MySingleton instance4 = MySingleton.GetInstance();
+            Console.WriteLine($"Intancia 4: {instance4.Id}");
+
+        }
+    }
+}
