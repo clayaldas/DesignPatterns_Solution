a8f053cdde88539a4bef2be9dcc66154e152716a
Clay Aldas <clayaldas@gmail.com>
2025-10-21 13:03:20 -0500

    Terminada clase del martes 21-octubre-2025

--- DesignPatternsImplementation/obj/Debug/net8.0/DesignPatternsImplementation.AssemblyInfo.cs
+++ DesignPatternsImplementation/obj/Debug/net8.0/DesignPatternsImplementation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPatternsImplementation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a18cdda768c7d10685bab6ad9fbc57728cf45c4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPatternsImplementation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPatternsImplementation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Utilities.DesignPatterns/Singleton/Log.cs
@@ -0,0 +1,26 @@
+﻿namespace Utilities.DesignPatterns.Singleton;
+
+// Patrón: Singleton
+// Aplicación utilizando un Log
+public sealed class Log
+{
+    private static Log? _instance;
+    //private string _nameFile = "log.txt";
+    private string _nameFile;
+    private Log(string nameFile)
+    {
+        _nameFile = nameFile;
+    }
+    public static Log GetIntance(string nameFile)
+    {
+        if (_instance == null)
+        {
+            _instance = new Log(nameFile);
+        }
+        return _instance;
+    }
+    public void SaveFile(string message)
+    {
+        File.AppendAllText(_nameFile, message + Environment.NewLine);
+    }
+}
